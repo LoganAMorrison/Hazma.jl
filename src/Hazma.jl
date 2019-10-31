71de744c31e41901267d91821bd67722fc7c152e
Adam Coogan <dr.adam.coogan@gmail.com>
2019-11-01 00:01:08 +0100

    export theory interface, scalar stuff, constants

--- src/Hazma.jl
+++ src/Hazma.jl
@@ -3,42 +3,42 @@ module Hazma
 using FastGaussQuadrature
 
 include("constants.jl")
-export ELECTRON_MASS;
-export MUON_MASS5;
-export NEUTRAL_PION_MASS;
-export CHARGED_PION_MASS;
-export NEUTRAL_KAON_MASS;
-export LONG_KAON_MASS;
-export CHARGED_KAON_MASS;
-export ETA_MASS;
-export ETA_PRIME_MASS;
-export RHO_MASS;
-export OMEGA_MASS;
-export CHARGED_B_MASS;
-export PION_MASS_CHIRAL_LIMIT;
-export KAON_MASS_CHIRAL_LIMIT;
-export UP_QUARK_MASS;
-export DOWN_QUARK_MASS;
-export STRANGE_QUARK_MASS;
-export CHARM_QUARK_MASS;
-export BOTTOM_QUARK_MASS;
-export TOP_QUARK_MASS;
+export me, ELECTRON_MASS;
+export mμ, MUON_MASS;
+export mπ⁰, NEUTRAL_PION_MASS;
+export mπ, CHARGED_PION_MASS;
+export mk⁰, NEUTRAL_KAON_MASS;
+export mkL, LONG_KAON_MASS;
+export mk, CHARGED_KAON_MASS;
+export mη, ETA_MASS;
+export mη′, ETA_PRIME_MASS;
+export mρ, RHO_MASS;
+export mω, OMEGA_MASS;
+export mB, CHARGED_B_MASS;
+export mΠ, PION_MASS_CHIRAL_LIMIT;
+export mK, KAON_MASS_CHIRAL_LIMIT;
+export mu, UP_QUARK_MASS;
+export md, DOWN_QUARK_MASS;
+export ms, STRANGE_QUARK_MASS;
+export mc, CHARM_QUARK_MASS;
+export mb, BOTTOM_QUARK_MASS;
+export mt, TOP_QUARK_MASS;
 export CM_TO_INV_MEV;
 export SIGMAV_INV_MEV2_TO_CM3_PER_S;
-export ALPHA_EM;
+export αem, ALPHA_EM;
 export GF;
 export VH;
 export CMB_TEMPERATURE;
 export PLANK_MASS;
 export CRITICAL_ENERGY_DENSITY;
 export SM_ENTROPY_DENSITY_TODAY;
-export OMEGA_H2_CDM;
+export Ω_H²_CDM, OMEGA_H2_CDM;
 export QU;
 export QD;
 export QE;
-export NEUTRAL_PION_DECAY_CONSTANT;
-export CHARGED_PION_DECAY_CONSTANT;
-export CHARGED_KAON_DECAY_CONSTANT;
+export fπ⁰, NEUTRAL_PION_DECAY_CONSTANT;
+export fπ, CHARGED_PION_DECAY_CONSTANT;
+export fk, CHARGED_KAON_DECAY_CONSTANT;
 export B0;
 
 include("decay/muon.jl")
@@ -48,5 +48,9 @@ export decay_spectrum_neutral_pion
 include("decay/charged_pion.jl")
 export decay_spectrum_charged_pion
 
+include("theory.jl")
+export list_annihilation_final_states, σ_χχ, br_χχ, dnde_γ, lines_γ, dnde_e, lines_e
+include("scalar.jl")
+export ScalarMediator, HiggsPortal, HeavyQuark
 
 end # module
