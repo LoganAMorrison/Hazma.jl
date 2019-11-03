3c4d9a561bcc8c9c75c1c761458a51fb3109352e
Logan A Morrison <loanmorr@ucsc.com>
2019-11-03 01:23:31 -0700

    Fixing typo in neutral-pion decay spectrum with mπ instead of mπ⁰

--- src/decay/neutral_pion.jl
+++ src/decay/neutral_pion.jl
@@ -5,6 +5,6 @@ Returns the radiative decay spectrum value from neutral pion given a γ-ray
 energy `eγ` and neutral pion energy `eπ`.
 """
 function decay_spectrum_neutral_pion(eγ::Real, eπ::Real)
-    eπ < NEUTRAL_PION_MASS && return zero(typeof(eγ))
-    br_π⁰_γγ * boosted_delta_function(eπ, mπ, eγ, zero(typeof(eγ)), mπ / 2)
+    eπ < mπ⁰ && return zero(typeof(eγ))
+    br_π⁰_γγ * boosted_delta_function(eπ, mπ⁰, eγ, zero(typeof(eγ)), mπ⁰ / 2)
 end
