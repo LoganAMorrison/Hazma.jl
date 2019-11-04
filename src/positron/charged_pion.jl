4c5a05ae8e879757ee5ddd8f4d163e8e0301aa16
Logan A Morrison <loanmorr@ucsc.com>
2019-11-03 23:01:46 -0800

    Formatting and removing old dndeₑ_π_decay

--- src/positron/charged_pion.jl
+++ src/positron/charged_pion.jl
@@ -7,24 +7,12 @@ the charged-pion given an arbitrary charged-pion energy `eπ`.
 function dndeₑ_π_decay(ep::Real, eπ::Real)
     eπ < mπ && return 0.0
 
-    # Line contributions from π → e ν
-    line = boosted_delta_function(eπ, mπ, ep, me, (mπ^2 + me^2) / (2 * mπ))
-    # Continuum spectrum from π → μν -> e ν ν ν
-    spectrum_rf(ep) = dndeₑ_μ_decay(ep, (mπ^2 + mμ^2) / (2 * mπ))
-    continuum = boost_spectrum(spectrum_rf, eπ, mπ, ep, me)
-
-    br_π_eν * line + br_π_μν * continuum
-end
-
-function dndeₑ_π_decay2(ep::Real, eπ::Real)
-    eπ < mπ && return 0.0
-
     # compute bounds on integration
     eμ_πrf = (mπ^2 + mμ^2) / (2 * mπ)
     γμ = eμ_πrf / mμ
     βμ = sqrt(1 - mμ^2 / eμ_πrf^2)
     ub = γμ * (me^2 + mμ^2 + βμ * sqrt((me^2 + mμ^2)^2 - 4 * me^2 * mμ^2)) /
-         (2 * mμ)
+             (2 * mμ)
 
     # Line contributions from π → e ν
     line = boosted_delta_function(eπ, mπ, ep, me, (mπ^2 + me^2) / (2 * mπ))
@@ -34,3 +22,5 @@ function dndeₑ_π_decay2(ep::Real, eπ::Real)
 
     br_π_eν * line + br_π_μν * continuum
 end
+
+@benchmark dndeₑ_π_decay(1.5mπ, 2mπ)
